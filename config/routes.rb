624c30994f1103ce1fb14b7da3a92cd700fd8453
MayuYazawa <121260144+MayuYazawa@users.noreply.github.com>
2023-04-14 04:55:40 +0000

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
